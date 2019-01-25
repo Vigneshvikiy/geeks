35f276a1d97e799a02bc56e66abc3d5b550d0080
Vigneshvikiy <46769037+Vigneshvikiy@users.noreply.github.com>
2019-01-25 21:00:56 +0530

    Create number.py

---
+++ number.py
@@ -0,0 +1,8 @@
+num=int(input("enter numbers:"))
+if num>0:
+         print("positive")
+         elif num=0:
+                    print("zero")
+                    else:
+                         print("negative")
+                         
