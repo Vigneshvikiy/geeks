307795d9c749849e3c55fcdc823d63451d8a4667
Vigneshvikiy <46769037+Vigneshvikiy@users.noreply.github.com>
2019-01-25 21:09:11 +0530

    Create READ.py

---
+++ READ.py
@@ -0,0 +1,7 @@
+num=int(input(enternumber:"))
+if num>0:
+        print("postive")
+        elif num=0:
+                   print("zero")
+                   else:
+                        print("negative")
